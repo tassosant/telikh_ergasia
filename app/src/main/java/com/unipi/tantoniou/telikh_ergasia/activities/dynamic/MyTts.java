50a1876b41aa2139765d53505d1055791b6ff1d6
evatherm90 <eva_therm90@hotmail.com>
2024-03-04 21:56:31 +0200

    text to speech class

---
+++ app/src/main/java/com/unipi/tantoniou/telikh_ergasia/activities/dynamic/MyTts.java
@@ -0,0 +1,24 @@
+package com.unipi.tantoniou.telikh_ergasia.activities.dynamic;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+
+public class MyTts {
+
+    private TextToSpeech tts;
+    TextToSpeech.OnInitListener  initListener= new TextToSpeech.OnInitListener() {
+        @Override
+        public void onInit(int status) {
+            if (status==TextToSpeech.SUCCESS){
+                //tts.setLanguage(Locale.forLanguageTag("el"));
+            }
+        }
+    };
+    public MyTts(Context context){
+        tts = new TextToSpeech(context,initListener);
+    }
+
+    public void speak(String message){
+        tts.speak(message,TextToSpeech.QUEUE_ADD,null,null);
+    }
+}
