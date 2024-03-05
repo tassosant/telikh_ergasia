549fd67afe0af8b951aa17e68b1acf528f7d71ac
evatherm90 <eva_therm90@hotmail.com>
2024-03-05 22:34:42 +0200

    addition of favourite story, additions in statistics activity

--- app/src/main/java/com/unipi/tantoniou/telikh_ergasia/activities/dynamic/StatisticsActivity.java
+++ app/src/main/java/com/unipi/tantoniou/telikh_ergasia/activities/dynamic/StatisticsActivity.java
@@ -2,12 +2,25 @@ package com.unipi.tantoniou.telikh_ergasia.activities.dynamic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.ArrayMap;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.unipi.tantoniou.telikh_ergasia.R;
 import com.unipi.tantoniou.telikh_ergasia.customActivity.CustomMenuActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class StatisticsActivity extends CustomMenuActivity {
+
+    SharedPreferences preferences;
+    ImageView image;
+    TextView favouriteStory;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +28,52 @@ public class StatisticsActivity extends CustomMenuActivity {
         setContentView(R.layout.activity_statistics);
         drawMenu(R.id.drawerLayout_StatisticsActivity);
         addNavOnClickEventListeners();
+
+        image = findViewById(R.id.storyImage2);
+        image.setImageDrawable(getResources().getDrawable(R.drawable.home_image));
+
+
+        favouriteStory = findViewById(R.id.textView3);
+        favouriteStory.setText("You have no favourite stories yet!");
+        textView = findViewById(R.id.textView);
+        textView.setText("You have no favourite stories yet!");
+        getStats();
     }
+
+    public void getStats(){
+        preferences = getSharedPreferences("com.unipi.tantoniou.telikh_ergasia", MODE_PRIVATE);
+
+        Map<String, Integer> statistics = new ArrayMap<>();
+        statistics.put("Story1", preferences.getInt("Story1", 0));
+        statistics.put("Story2",preferences.getInt("Story2", 0));
+        statistics.put("Story3",preferences.getInt("Story3", 0));
+        statistics.put("Story4",preferences.getInt("Story4", 0));
+        statistics.put("Story5",preferences.getInt("Story5", 0));
+        statistics.put("Story6", preferences.getInt("Story6", 0));
+        StringBuffer text = new StringBuffer();
+
+        for (String key : statistics.keySet()) {
+            text.append(key + ": " + statistics.get(key)+" times\n");
+        }
+        textView.setText(text);
+        String favourite = getFavourite(statistics);
+        favouriteStory.setText(favourite);
+    }
+
+    public String getFavourite( Map<String, Integer> statistics){
+
+        String maxSt="You have no favourite stories yet!";
+        Integer max=0;
+        for (String key : statistics.keySet()) {
+            if (max<statistics.get(key)){
+                maxSt = key;
+                max = statistics.get(key);
+            }
+        }
+        if (max==0){
+            return maxSt;
+        }
+        return "Your favourite story is: "+ maxSt + "\nYou have heard it " + max+" times!";
+    }
+
 }
